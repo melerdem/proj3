59bacfc630895989290185392955f95b7e84a22b
melerdem <149095031+melerdem@users.noreply.github.com>
2025-04-22 23:15:24 -0400

    Update API_PyTest.py (so that it returns for GUI)

--- API_PyTest.py
+++ API_PyTest.py
@@ -4,155 +4,63 @@
 #         https://pypi.org/project/wonderwords/
 
 import requests
-import json
 import wikipediaapi
+
+# --- Get Wikipedia summary ---
+def get_wikipedia(wikiPage):
+    wikiAccessAgent = wikipediaapi.Wikipedia(user_agent='API_Test (gabrielfernandez@example.com)', language='en')
+    return wikiAccessAgent.page(wikiPage)
+
+# --- Get dictionary definitions ---
+def get_dictionary(dictionarySetter):
+    try:
+        dictionaryData = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{dictionarySetter}").json()
+        definitions = dictionaryData[0]["meanings"][0]["definitions"]
+        return [d["definition"] for d in definitions]
+    except Exception:
+        return ["No definitions found."]
+
+# --- Get thesaurus synonyms and antonyms ---
+def get_thesaurus(dictionarySetter):
+    try:
+        dictionaryData = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{dictionarySetter}").json()
+        synonyms = dictionaryData[0]["meanings"][0].get("synonyms", [])
+        antonyms = dictionaryData[0]["meanings"][0].get("antonyms", [])
+        return synonyms, antonyms
+    except Exception:
+        return [], []
+
+# --- Optional: Get a random word (if you still use wonderwords) ---
 from wonderwords import RandomWord
 
-def get_wikipedia(wikiPage):
-    wikiAccessAgent = wikipediaapi.Wikipedia(user_agent='API_Test (gabrielfernandez@ufl.edu)', language='en')
-
-    try:
-        page = wikiAccessAgent.page(wikiPage)
-
-    except:
-        print("The Wikipedia page could not be accessed")
-        #page = wikiAccessAgent.page("error")
-
-    return page
-
-def get_dictionary(dictionarySetter):
-    dictionaryData = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{dictionarySetter}").json()
-
-    # Select in each data structure by naming one of the entries (like sub directories)
-    print(dictionaryData[0]["word"] + " " + "(Definition)")
-    print("--------------------------------------------------")
-
-    # List all of the different definitions
-    counter = 0
-    for i in dictionaryData[0]["meanings"][0]["definitions"]:
-        counter += 1
-        print(counter, end=".) ")
-        print(i["definition"], end="\n\n")
-
-    return dictionarySetter
-
-def get_thesaurus(dictionarySetter):
-    dictionaryData = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{dictionarySetter}").json()
-    #thesaurusData = Thesaurus(dictionarySetter)
-
-    # Select the synonyms (and antonyms)
-    # Synonyms
-    try:
-        numSym = len(dictionaryData[0]["meanings"][0]["synonyms"])
-        
-        
-        print(dictionarySetter + " " + "(Synonyms)")
-        print("--------------------------------------------------")
-            
-        for synonym in dictionaryData[0]["meanings"][0]["synonyms"]:
-            print(synonym, end="\n\n")
-        print()
-
-    except:
-        print("No synonym entry found in Thesaurus", end="\n\n")
-
-    print()
-
-    # Antonyms
-    try:
-        numAnt = len(dictionaryData[0]["meanings"][0]["synonyms"])
-
-        print(dictionaryData[0]["word"] + " " + "(Antonyms)")
-        print("--------------------------------------------------")
-
-        for antonym in dictionaryData[0]["meanings"][0]["antonyms"]:
-            print(antonym, end="\n\n")
-        print()
-
-    except:
-         print("No antonym entry found in Thesaurus", end="\n\n\n")
-
-# Separating this into an indivudal function may never be necessary, but it should
-# be useful if we need to get a random word any time other than at the beginning of
-# inputting a new word
-def generate_randWord(randomizer):
+def generate_randWord():
+    randomizer = RandomWord()
     return randomizer.word()
 
-
+# --- Optional: Manual test (CLI) ---
 def main():
-    randomizer = RandomWord()
-    stopProgram = False
-    counter = 0
+    while True:
+        query = input("Enter a word (or 'exit'): ").strip()
+        if query.lower() == "exit":
+            break
+        if query.lower() == "random":
+            query = generate_randWord()
+            print(f"Random word: {query}")
 
-    while (stopProgram == False):
-        wasSuccessful = False
+        print("\n--- Wikipedia ---")
+        page = get_wikipedia(query)
+        print(page.summary if page.exists() else "No Wikipedia entry found.")
 
-        # User Input
-        print("Input: ",end="")
-        queryPhrase = input()
+        print("\n--- Dictionary ---")
+        definitions = get_dictionary(query)
+        for i, d in enumerate(definitions, 1):
+            print(f"{i}. {d}")
 
-        if (queryPhrase == "do random"):
-            queryPhrase = randomizer.word()
-        if (queryPhrase == "stop program"):
-            stopProgram = True
-
-        print()
-
-        if (stopProgram == False):
-            # Perform Wikipedia checking operation
-            try:
-                page = get_wikipedia(queryPhrase)
-
-                if page.exists():     
-                    # Wikipedia
-                    print(page.title + " " + "(Wikipedia)")
-                    print("--------------------------------------------------")
-                    print(page.summary)
-                    print()
-
-                    wasSuccessful = True
-                else:
-                    print('Failed to fetch page from API.')
-
-                    print()
-            except:
-                print("No entry found on Wikipedia")
-
-            # Divide the text
-            print()
-            print("- - - - - - - - - -")
-            print('\n')
-
-            # Perform the Dictionary checking operation
-            try:
-                get_dictionary(queryPhrase)
-                wasSuccessful = True
-            except:
-                print("No entry found in Dictionary")
-    
-            # Divide the text
-            print('\n')
-            print("- - - - - - - - - -")
-            print('\n')
-
-            # Perform the Thesaurus checking operation
-            try:
-                get_thesaurus(queryPhrase)
-                # Assume that there is no case where dictionary finds nothing, syn finds nothing, but ant finds something
-            except:
-                print(end="")
-            
-            # If you want to add data to a structure, do it here, as it will exclude spam entries which find no results
-            if (wasSuccessful == True):
-                counter += 1
-            
-            print("Enter \"stop program\" in order to end")
-            print("Enter \"do random\" for a random word")
-            print("Total entries searched in this instance:", end=" ")
-            print(counter)
-
-            # Print again to separate the request to press a key to continue the program
-            print("\n\n")
+        print("\n--- Thesaurus ---")
+        syns, ants = get_thesaurus(query)
+        print("Synonyms:", ", ".join(syns) if syns else "None")
+        print("Antonyms:", ", ".join(ants) if ants else "None")
+        print("\n" + "-" * 50 + "\n")
 
 if __name__ == '__main__':
     main()
